070b5ac17192612e84f06a6415de55394fa6e5ab
João Marcos <jmarcossferreira@hotmail.com>
2022-09-27 15:06:45 -0300

    Create simple form page

---
+++ src/pages/Form/components/TellMeMore/index.tsx
@@ -0,0 +1,24 @@
+import React, { ChangeEvent, useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+
+export const TellMeMore = () => {
+  const { register, unregister } = useFormContext();
+  const [showInput, setShowInput] = useState(false);
+
+  const handleOnChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    setShowInput(target.checked);
+    if (!target.checked) {
+      unregister('tellMeMore');
+    }
+  };
+  return (
+    <div className="flex flex-col">
+      <label htmlFor="tellMoreToggle" className="pb-4">
+        <input type="checkbox" id="tellMoreToggle" onChange={handleOnChange} />
+        <span className="text-white pl-2">Plz tell us more</span>
+      </label>
+
+      {showInput && <textarea id="tellMore" {...register('tellMeMore')} />}
+    </div>
+  );
+};
--- src/pages/Form/index.tsx
+++ src/pages/Form/index.tsx
@@ -1,5 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm, FormProvider } from 'react-hook-form';
+import { TellMeMore } from './components/TellMeMore';
+
+interface FormWithContextProps {
+  name: string;
+  email: string;
+  age: string;
+  tellMeMore: string;
+}
 
 export const FormPage = () => {
-  return <div>FormPage</div>;
+  const { ...methods } = useForm<FormWithContextProps>();
+
+  const onSubmit = (data: FormWithContextProps) => {
+    console.log(data);
+    methods.reset();
+  };
+
+  return (
+    <section>
+      <h1 className="text-center text-xl mb-8">
+        Form with useFormContext and conditional Field
+      </h1>
+      <FormProvider {...methods}>
+        <form
+          className="items-center flex flex-col gap-2 text-slate-800"
+          onSubmit={methods.handleSubmit(onSubmit)}
+        >
+          <input
+            type="text"
+            id="name"
+            placeholder="Name"
+            {...methods.register('name')}
+          />
+          <input
+            type="email"
+            id="email"
+            placeholder="E-mail"
+            {...methods.register('email')}
+          />
+          <input
+            type="number"
+            id="age"
+            placeholder="Age"
+            {...methods.register('age')}
+          />
+          <TellMeMore />
+          <button className="bg-blue-600 rounded-sm shadow-sm px-4 py-2">
+            Save
+          </button>
+        </form>
+      </FormProvider>
+    </section>
+  );
 };
