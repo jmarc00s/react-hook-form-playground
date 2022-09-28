9f212d4dfa61ea8071ad0ab0a3ec7573f4d65a88
João Marcos <jmarcossferreira@hotmail.com>
2022-09-28 17:35:22 -0300

    Create remove field button

--- src/pages/FormArray/index.tsx
+++ src/pages/FormArray/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useFieldArray, useForm } from 'react-hook-form';
 
 interface FormProps {
@@ -8,7 +8,7 @@ interface FormProps {
 export const FormArrayPage = () => {
   const { register, control, handleSubmit } = useForm();
 
-  const { fields, append } = useFieldArray({
+  const { fields, append, remove } = useFieldArray({
     name: 'test',
     control,
   });
@@ -20,23 +20,31 @@ export const FormArrayPage = () => {
   const onSubmit = (data: any) => console.log(data);
 
   return (
-    <section className="flex flex-col items-center ">
+    <section className="flex flex-col w-1/2 mx-auto">
       <form onSubmit={handleSubmit(onSubmit)}>
         <h1>Form array</h1>
         <div className="flex flex-col">
           {fields.map((field, index) => {
             return (
-              <input
-                className="text-slate-800"
-                key={field.id}
-                type="text"
-                placeholder="Value"
-                {...register(`test.${index}.value` as const)}
-              />
+              <div key={field.id} className="py-2">
+                <input
+                  className="text-slate-800 w-3/4 mr-auto"
+                  key={field.id}
+                  type="text"
+                  placeholder="Value"
+                  {...register(`test.${index}.value` as const)}
+                />
+                <button
+                  className="bg-red-500 text-white w-1/4 h-10"
+                  onClick={() => remove(index)}
+                >
+                  Remove
+                </button>
+              </div>
             );
           })}
         </div>
-        <button className="text-white bg-blue-600 rounded-sm shadow-sm px-4 py-2 w-full">
+        <button className="text-white bg-blue-600 rounded-sm shadow-sm px-4 py-2 w-full ">
           Send
         </button>
       </form>
