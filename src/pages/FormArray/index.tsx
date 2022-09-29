0a6a7622329399f1dfb6bda64aab5ed755f60e33
João Marcos <jmarcossferreira@hotmail.com>
2022-09-29 16:17:07 -0300

    Improve form with array

--- src/pages/FormArray/index.tsx
+++ src/pages/FormArray/index.tsx
@@ -1,12 +1,19 @@
-import React, { useEffect } from 'react';
-import { useFieldArray, useForm } from 'react-hook-form';
+import React, { useEffect, useMemo } from 'react';
+import { useFieldArray, useForm, useFormState } from 'react-hook-form';
 
 interface FormProps {
   value: string;
 }
 
 export const FormArrayPage = () => {
-  const { register, control, handleSubmit } = useForm();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm({
+    mode: 'onChange',
+  });
 
   const { fields, append, remove } = useFieldArray({
     name: 'test',
@@ -21,32 +28,47 @@ export const FormArrayPage = () => {
 
   return (
     <section className="flex flex-col w-1/2 mx-auto">
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex items-center justify-between mb-4">
         <h1>Form array</h1>
+        <button
+          className="btn btn-primary mt-4 btn-sm"
+          onClick={handleCreateField}
+        >
+          Create field
+        </button>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="flex flex-col">
           {fields.map((field, index) => {
             return (
-              <div key={field.id} className="py-2">
+              <div key={field.id} className="py-2 flex">
                 <input
-                  className="input w-3/4"
+                  className="input flex-1"
                   key={field.id}
                   type="text"
                   placeholder="Value"
-                  {...register(`test.${index}.value` as const)}
+                  {...register(`test.${index}.value` as const, {
+                    required: true,
+                  })}
                 />
-                <button
-                  className="btn btn-outline btn-error w-1/4"
-                  onClick={() => remove(index)}
-                >
-                  Remove
-                </button>
+                <div className="px-1">
+                  <button
+                    className="btn btn-outline btn-error"
+                    onClick={() => remove(index)}
+                  >
+                    Remove
+                  </button>
+                </div>
               </div>
             );
           })}
         </div>
-        <button className="btn btn-primary btn-block">Send</button>
+        {!!fields.length && (
+          <button className="btn btn-success btn-block" disabled={!isValid}>
+            Send
+          </button>
+        )}
       </form>
-      <button onClick={handleCreateField}>Append</button>
     </section>
   );
 };
