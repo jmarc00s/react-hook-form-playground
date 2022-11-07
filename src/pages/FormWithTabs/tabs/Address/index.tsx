938d537053b63e02fa4416ff8f9bb742928710a4
João Marcos <jmarcossferreira@hotmail.com>
2022-11-07 17:53:35 -0300

    Create address form

--- src/pages/FormWithTabs/tabs/Address/index.tsx
+++ src/pages/FormWithTabs/tabs/Address/index.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import { Input } from 'components/Input';
+import { useTabsFormContext } from 'pages/FormWithTabs/utils/hooks/useTabsFormContext';
+import React, { ReactElement, useMemo } from 'react';
 
-const AddressTab = () => {
-  return <div>AddressTab</div>;
+const AddressTab = (): ReactElement => {
+  const {
+    register,
+    formState: { errors },
+  } = useTabsFormContext();
+
+  const streetErrors = errors?.address?.street?.message;
+  const neighborhoodErrors = errors?.address?.neighborhood?.message;
+
+  return (
+    <section className="flex flex-col">
+      <div className="flex gap-2">
+        <Input
+          label="Street"
+          {...register('address.street')}
+          required
+          errorMessage={streetErrors}
+        />
+        <Input
+          label="Neighborhood"
+          {...register('address.neighborhood')}
+          required
+          errorMessage={neighborhoodErrors}
+        />
+      </div>
+      <div className="flex gap-2">
+        <Input label="Zip code" {...register('address.zipCode')} />
+        <Input label="City" {...register('address.city')} />
+      </div>
+    </section>
+  );
 };
 
 export default AddressTab;
