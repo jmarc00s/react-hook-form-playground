71898cc2b6cd5706c2d646a418481c5d7cb8e420
João Marcos <jmarcossferreira@hotmail.com>
2022-11-09 16:57:53 -0300

    Improving toolbar

--- src/pages/FormWithTabs/components/Toolbar/index.tsx
+++ src/pages/FormWithTabs/components/Toolbar/index.tsx
@@ -5,6 +5,7 @@ import {
   XMarkIcon,
 } from '@heroicons/react/24/outline';
 import { useUiContext } from 'pages/FormWithTabs/utils/hooks/useUiContext';
+import { Badge } from 'components/Badge';
 
 export const Toolbar = (): ReactElement => {
   const { cancel, setAdding, formStatus } = useUiContext();
@@ -14,19 +15,22 @@ export const Toolbar = (): ReactElement => {
   const onCancelClick = () => cancel();
 
   return (
-    <div className="flex justify-center gap-2">
-      {JSON.stringify(formStatus)}
-      <button onClick={onNewClick} className="btn gap-2 btn-primary w-32">
-        <PlusIcon className="h-6 w-6" /> New
-      </button>
-      <button onClick={onSaveClick} className="btn btn-primary gap-2">
-        <ArrowUpOnSquareIcon className="h-6 w-6" />
-        Save
-      </button>
-      <button onClick={onCancelClick} className="btn btn-primary gap-2">
-        <XMarkIcon className="h-6 w-6" />
-        Cancel
-      </button>
+    <div className="flex justify-between items-center ">
+      <div className="flex gap-2">
+        <button onClick={onNewClick} className="btn gap-2 btn-primary w-32">
+          <PlusIcon className="h-6 w-6" /> New
+        </button>
+        <button onClick={onSaveClick} className="btn btn-primary gap-2">
+          <ArrowUpOnSquareIcon className="h-6 w-6" />
+          Save
+        </button>
+        <button onClick={onCancelClick} className="btn btn-primary gap-2">
+          <XMarkIcon className="h-6 w-6" />
+          Cancel
+        </button>
+      </div>
+      <Badge text={formStatus} size="lg" variant="secondary" />
+      <div></div>
     </div>
   );
 };
